cabd3be174d7470b896e97589a4e97da1c445904
vasupatel2344 <vasupatel2344@gmail.com>
2024-09-26 11:15:03 +0530

    move file

---
+++ 01_basics/index.js
@@ -0,0 +1,10 @@
+console.log("Vasu Patel");
+
+const accoundId = 1234;
+
+let accountEmail = "vasupatel@gmail.com";
+let accountPassword = "12345";
+accountcity = "rajkot";
+
+console.log(accountcity, accountEmail, accountPassword);
+console.table([accountEmail, accountPassword, accountcity]);
