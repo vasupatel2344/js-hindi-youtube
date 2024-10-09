4a7f653791adb19fb7f3ae3ee84da7f2f8de67b6
vasupatel2344 <vasupatel2344@gmail.com>
2024-10-09 11:46:13 +0530

    new code

---
+++ 01_basics/Practice.js
@@ -0,0 +1,816 @@
+console.log("Hello World");
+
+//1️⃣ challange time ⏺️
+
+var Myname ="vasu patel";
+// console.log(Myname);
+// console.log(Myname);
+// var _myName = "vasu";
+// var _1my__Name = "vasu patel";
+// var 1myName = "vasu vegda";
+// var $myName = "the vasu";
+
+// console.log(_myName); 
+
+// var myName = "vasu patel";
+// console.log(myName);
+
+// console.log(typeof(myName));
+
+
+// var myAge = 19;
+// console.log(myAge);
+
+// console.log(typeof(myAge));
+
+// var iAmVegda = true 
+// console.log(iAmVegda);
+
+// console.log(typeof(iAmVegda));
+
+// // var myAge = "20";
+// // console.log(myAge);
+
+// var iamvegda; 
+
+// console.log(typeof(iamvegda));
+
+
+// // 2️⃣ challenge time ⏺️
+
+// 10 + "20"
+// console.log(10 + "20");
+// 9 - "5"
+// console.log(9 - "5");
+// "java" + "script"
+// console.log("java" + "script");   //(- nan)
+// " " + " "
+// console.log("" + "");
+// " " + 0
+// console.log(" " - 0);
+// "vasu" + "patel"
+// console.log( "vasu" + "patel");
+// true + true
+// console.log(true + true);
+// true + false
+// console.log(true + false);
+// false + false
+// console.log(false + false);
+// false + true
+// console.log(false + true);
+
+
+
+// // 🔴🔴interview qustion 1 
+// // Difference Between null and undefined👈🏻
+
+// var iAmUseless = null
+// console.log(iAmUseless);
+// console.log(typeof(iAmUseless));
+
+// 2nd javascript bug
+
+// var iAmStandby;
+// console.log(iAmStandby);
+// console.log(typeof(iAmStandby));
+
+// var kjasfbvuergergno;
+// console.log(kjasfbvuergergno);
+// console.log(typeof(kjasfbvuergergno));
+
+// interview question //NaN
+
+// var myPhoneNumber = "vasu"
+// var myName = 7935632
+
+// console.log(isNaN(myPhoneNumber));
+// console.log(isNaN(myName));
+// if (isNaN(myName)){
+//   console.log("plese enter valid Number");
+// }
+// else{
+//   console.log("not found");
+// }
+// console.log(typeof myPhoneNumber);
+
+
+
+// 🔴🔴interview qustion 2
+// what is nan👈🏻
+
+NaN === NaN;
+Number.NaN === NaN;
+isNaN(NaN);
+isNaN(Number.NaN) ;
+Number.isNaN(NaN);
+
+// console.log(isNaN(Number.NaN));
+
+// 1️⃣ assingment operators
+
+var x = 5;
+var y = 5;
+
+// console.log("is the box x and y f equal " + x == y);
+// // i will tell you when we wiil see es6
+// console.log(`is the box x and y are equal  : ${x == y} `);
+
+
+// 2️⃣ arithmetic and operator👈🏻
+
+// console.log(3+3);
+// console.log(10-5);
+// console.log(20/5);
+// console.log(5*6);
+
+// console.log("reminder operator " + 81%8);
+
+
+// ✅ increment and decrement operator
+
+// 🔴 increment operator
+debugger;
+var num =15;
+var newNum = num++ ;  // [post fixe] (num +5)
+// console.log(num);
+// console.log(newNum);
+
+
+
+// 🔴 prefix increment operator
+
+var num = 15;
+var newNum = ++num;
+// console.log(num);
+// console.log(newNum);
+
+// 🔴 decrement operator
+
+// var num = 15;̣̣
+// var newNum = num--;
+// console.log(num);  
+// console.log(newNum);
+
+// prefix decrement operator
+
+// var num = 15;
+// var newNum =--num;
+// console.log(num);
+// console.log(newNum);
+
+//    Expression and operaotrs👈🏻
+
+// 3️⃣-comparition operators👈🏻
+
+var a = 30;
+var b = 20;
+  
+// equal (==)4
+console.log(a == b);
+// not equal(a != b)
+console.log(a != b);
+
+// greater then (>)
+// console.log(a > b);
+
+// greater then to equal(>=)
+// console.log(a >= b);
+
+// less then (<)
+// console.log(a < b);
+
+// console.log(a <= b);
+
+
+
+// 4️⃣logical operators
+
+// when they are they return boolean value
+//
+var a = 30;
+var b = 620;
+
+// console.log(a > b);
+// console.log(a < b);
+
+// logical and op.(&&)
+// all op. is true & true
+// console.log((a > b) && (b > 0) && (b < 0));
+
+// logical or op. (||)
+// the logical or(||) operator
+// console.log((a > b) || (b > 0) || (b < 0));
+
+//logical not op. (!)
+// takes true to falsity and vise versa.
+// console.log(!((a < 0) || (a < 0) || (b > 0 )));
+// console.log(!true);
+
+
+
+// 5️⃣ String concatenation (operators) 
+
+
+// console.log("hello world");
+// console.log("Hello "+"world");
+// console.log("vasu "+"patel");
+// var Myname = "vasu   ";
+// console.log(Myname + " vegda");
+
+
+
+// 4️⃣ challange time 
+
+
+// // sol-1✔️;
+// console.log(3*2); 
+// console.log(4*3);
+// console.log(5**5);
+// console.log(10**3);
+
+
+// // sol-2✔️;
+// console.log( 9 + " vasu");
+// console.log( 8 - "vasu");
+
+
+// sol-3✔️;
+var a = 5;
+var b = 10;
+
+// output a = 10 b =5;
+// var c = b;  // c=10;
+// b = a; //b = 5
+// a = c;
+// console.log("the value of a is " + a);
+// console.log("the value of b is " + b);
+
+
+//sol -4✔️; 
+
+var a = 5;
+var b = 10;
+
+// output a = 10 b =5;
+
+// a = a+b; // a= 15;
+// b = a-b; // b= 5;
+// a = a-b; // a= 10; 
+
+// console.log("the value of a is " + a);
+// console.log("the value of b is " + b);
+
+
+var num1 = 100;
+var num2 = "100"
+
+// console.log(num1);
+// console.log(num2);
+
+
+// // 3:- interview qustion
+
+// var num1 = '1';
+// var num2 = '5';
+// console.log(typeof(num1));
+// console.log(typeof(num2));
+// console.log(num1 === num2); //5
+
+
+// var num1 = 5;
+// var num2 = '5'
+// console.log(typeof(num1));
+// console.log(typeof(num2));
+// console.log(num1 == num2); //true
+
+// var num1 = 5;
+// var num2 = '5'
+// console.log(typeof(num1));
+// console.log(typeof(num2));
+// console.log(num1 === num2); //false
+
+
+//4️⃣control statement & loops
+
+// ⏺️1️⃣= if ... else
+
+// if raining = raincoat
+  // else no raincoat");
+
+// var tomr = 'sunny';
+
+// if (tomr == 'sunny'){
+//   console.log("take a raincot");
+// }else {
+//   console.log("no need to take a raincot");
+// }
+
+
+// challange time
+
+// var year = 2020;
+
+// if (year % 4){
+//   if (year % 100){
+//     if(year % 400){
+//       console.log("the year" + year + "is a leap year");
+//     }else{
+//       console.log("the year ") + year + " is not a leap year";
+//     }
+//   }else{
+//     console.log("the year " + year + " is a leap year");
+//   }
+// }else{
+//   console.log("the year" + year + "is not a leap year");
+// }
+
+// console.log(year + " is a leap year");
+
+
+// what is a truthy and falsy value in java script
+
+// 0, "" , nan , null , undefined , false** is false anyway
+
+// if (score = null) {
+//   console.log("yah we won the game 😃");
+// } else {
+//   console.log("OMG we less the game 😭");
+// }
+
+
+// 2️⃣ condition (ternary) operator
+
+// that takes three operands
+var age = 18;
+// if (age < 181){
+//     console.log("you are eligible to vote");
+// } else {
+//     console.log("you are not eligible to vot");
+// // }
+
+// var age = 17;
+
+// console.log((age >= 18 ) ?"you can vote" : "you can't vote");
+
+
+
+// 3️⃣ switch Statement
+// 1st withaot break statement
+// find the arwa of cricle tringlr and rectanglr?
+
+// var area = circle;
+// var pi = 3.142, l=5 , b= 4, r =3;
+
+// if (area = "circle"){
+//   console.log("the area of the circle is : " + pi*r**2);
+// }else if (area == "triangle"){
+//   console.log("the area of the triangle is : " + (l*b)/2);
+// }else if (area == "rectangle"){
+//   console.log("the area of the triangle is : " + (l*b));
+// }else{
+//   console.log("please enter valid data");
+// }
+
+// // switch statement 
+
+var area = "circle";
+var pi = 3.142, l=5 , b= 4, r =3;
+// switch (area){
+//   case 'circle':
+//       console.log("the area of the circle is : " + pi*r**2);
+//     case 'triangle':
+//       console.log("the area of the triangle is : " + (l*b)/2);
+//     case 'rectangle' :
+//       console.log("the area of the triangle is : " + (l*b));
+//       default :
+//       console.log("please enter valid data");
+// }
+// block scope
+var num = 0; // inisyal value
+debugger;
+while (num <= 100) {
+    console.log(num++); //infinte loop
+  num++
+}
+
+// // 5️⃣ do - while - loop stement
+
+var num  = 300;
+// // block scope  
+do{
+//      console.log(num);  //infinte loop
+  num++
+}while (num <= 1000);
+
+
+// 6️⃣ for loop
+debugger;
+for(var num = 10; num <= 100; num++){
+    // console.log(num)
+}
+
+
+
+// ✅ 6 - challenge time
+// java script program to print table for given number(8)?
+// debugger;
+// for (var num = 1; num <= 10; num++){
+//     var tableof = 8;
+//     console.log(tableof + " + " + num + " = " + tableof * num);
+//     }
+///////////////////////////////////////////////////////////
+
+// 5️⃣ function in javascript
+
+//  a java script finction block of code designed to perform a prticular task.
+
+//   1️⃣ function definition
+
+
+// before we use a function, we need to difine it
+
+//what we will see
+// function definition
+
+
+// var a = 10;
+// var b = 20;
+// var sum = a+b;
+// console.log(sum);
+
+
+// function sum(){
+//     var a = 10, b = 20;
+//     var total = a + b;
+//     console.log(total);
+// }
+
+
+// 2️⃣ calling function
+
+function sum(){
+  var a = 10, b = 20;
+  var total = a + b;
+  console.log(total);
+}
+sum()
+
+// 3️⃣ function parameter VS function arguments
+
+// function sum (a,b){
+// var total= (a+b)
+// console.log(total);
+// }
+
+// sum()
+// sum(10,10)
+// sum(30,40)
+
+
+//4️⃣ functions expressions
+// function expressions 
+// "creat a function and put it into the variable"
+
+// function sum(a,b){
+//     var total = a + b;
+//     console.log(total);
+// }
+// var fun = sum(100,5);
+// var funExp = sum(5,103);
+// var funExp = sum(5,130);
+// var funExp = sum(5,113);
+// var funExp = sum(5,110);
+
+
+// 5️⃣ returen keyword
+
+// function sum(a,b){
+//    return total = a + b;
+// }
+// var funExp = sum(5,13);
+
+// console.log(funExp);
+
+
+// 6️⃣ Anonymous Function
+
+
+// var funExp = function (a,a){
+//   return total = a * a;
+// }
+//   var sum = funExp(15,15);
+//   var sum1 = funExp(25,25);
+
+//   console.log(funExp);
+
+//   console.log(sum);
+
+//   console.log('the sum of two no is ' + funExp);
+
+//   console.log(sum<sum1);
+
+///////////////////////////////////////////////////////////
+
+
+//✅ WELCOME TO ECMA Script
+
+// 1️⃣   LET VS CONST VS  VAR
+
+// var =>Function scope
+// let and const => block scope
+
+
+
+
+// var myName = "vasu patel";
+// console.log(myName);
+
+// let myaName = "vasu vegda";
+// console.log(myaName);
+
+// const myName = "vasu vegda";
+// console.log(myName);
+
+// debugger;
+// function biodata(){
+//    var myFirstName = "vasu vegda";
+//    console.log(myFirstName);
+
+//    if (true){
+//       var myLastName = "vasu patel";
+//       console.log("inner " + myFirstName);  
+//       console.log("inner " + myLastName);
+//    }
+//    console.log("innerouter " + myLastName);
+// }
+// biodata(); 
+
+// 2️⃣ temeplate literals temeplate srin
+// java script program to print table to given nuber (8)?
+
+// for (let num = 1; num <=10; num++){
+//   let tableof = 17;
+//   console.log(tableof + " * " + num + " = " + tableof * num);
+//   console.log( `${tableof} * ${num} * = ${tableof * num}` );
+// }  
+
+// 3️⃣ default parameters
+
+// // dafult function parameters allowd named parametars to be initializedwithe dafilt value of no value or undefined is passed
+
+// function mult (a,b){
+//    return a*b;
+// }
+// console.log(mult(5,5));
+
+
+// function mult (a,b = 9){
+//    return a*b;
+// }
+// console.log(mult(5));
+
+
+//4️⃣
+//5️⃣
+//6️⃣ fat array function
+// normal way to writing function 
+
+// console.log(sum());
+// function sum(){
+//   let a = 5 , b = 6;
+//   let sum =  a+b;
+//   return `the sum of the two number is ${sum}`;
+//  }
+
+
+// const sum = () =>{
+//   let a = 5 , b = 60;
+//   let sum =  a+b;
+//   return  `the sum of the two number is ${sum}`; 
+// }
+
+// console.log(sum());
+
+//7️⃣
+//8️⃣
+//9️⃣
+
+//6️⃣ Array in jvascript
+
+// var MyFrinds = ["vasu",20, 'kushal',20, "jeel",22, "tej",20,true]
+// console.log(MyFrinds);
+
+  // 1️⃣ Array subsection travesal in Array
+
+var myfrinds = ['vasu','jeel','harsh','fevil']
+
+// console.log(myfrinds[myfrinds.length - 2]);
+
+// console.log(myfrinds.length);
+// console.log(myfrinds);
+
+// we use for loop to nevigate
+
+var myfrinds = ['vasu','jeel','harsh','fevil']
+
+// for(var i=0; i<myfrinds.length; i++){
+//   console.log(myfrinds[i]);
+// }
+
+// after es6 we and for in and for of loop to
+
+var myfrinds = ['vasu','jeel','harsh','fevil']
+
+// for (let elements of myfrinds){
+//   console.log(elements);
+// }
+
+// for (let elements in myfrinds){
+//   console.log(elements);
+// }
+
+// Array prototype forEach();
+// calls a functionfor each Element in the Array
+// normale tredishnal function
+
+// var myfrinds = ['vasu','jeel','harsh','fevil']
+
+// myfrinds .forEach(function (element , index , Array){
+//   console.log(element + " index : " + index + Array);  
+// });
+
+// or 
+
+// fet Array function
+
+// myfrinds .forEach((element , index , Array) => {
+//   console.log(element + " index : " + index + Array);  
+// });
+
+
+
+// 2️⃣ Array sunsection 2searching and filter in array
+
+   
+
+
+
+// 3️⃣  
+
+
+
+
+
+// // 4️⃣ Array subcection 4 👉🏻 perform CRUD   IMP
+
+// Array prototype .push("end edit") 
+
+// const bhajiya = ['thepla' , 'gathiya' , 'khaman'];
+
+// const count = bhajiya.push('dabeli');
+// console.log(bhajiya);
+// console.log(count);
+
+// &&&&&&&
+
+// const bhajiya1 = ['thepla' , 'gathiya' , 'khaman'];
+
+// const count = bhajiya1 .push('bhagiya')
+// console.log(bhajiya1);
+// console.log(count);
+
+/////////////
+
+// const bhajiya1 = ['thepla' , 'gathiya' , 'khaman'];
+// const count = bhajiya1 .unshift('bhagiya')
+
+// console.log(bhajiya1);
+// prototype . unshift ("unshift edit")
+// &&&&&&&&&&&&&
+// const http = ['mobile','leptop','ipad','dekstop']
+// const count = http .unshift('tablet')
+
+// console.log(http);
+
+//   2nd example
+
+// const myNumber = [1,2,3,4,5];
+
+// myNumber.unshift ('6,7');
+// console.log(myNumber);
+
+// ///////////// 
+// const bhajiya = ['thepla' , 'gathiya' , 'khaman'];
+
+// bhajiya.unshift  ('khakra' , 'khandvi' , 'dabeli');
+// console.log(bhajiya);
+
+// Array prototype . only shift 
+
+// const thebhajiya = ['thepla' , 'gathiya' , 'khaman' ,'a' , 'errfwr' , 'dd', 'wv'];
+
+// bhajiya.shift ()
+// console.log(thebhajiya);
+
+// // 2nd example
+
+// const myNumber = [1,2,3,4,5,6,8,0] 
+
+// myNumber.push (7,9)
+// console.log(myNumber);
+// ///////////////////////////////////////
+// const thesuccess = ['bellive' , 'discover' , 'yourself'];
+
+// thesuccess.unshift  ('thot' , 'goal' , 'time management')
+// console.log(thesuccess);
+
+// Array .prototype. pop()
+// // the pop() methodremovs the last element from an array and returen thet element. this methad chenges the length of the arrey 
+
+// const success = ['bellive' , 'discover' , 'yourself','thot' , 'goal' , 'time management'];
+
+// console.log(success);
+// console.log(success .pop());
+// console.log(success);
+
+// array prototype.shift(success); //
+
+
+// const success = ['bellive' , 'discover' , 'yourself','thot' , 'goal' , 'time management'];
+
+// console.log(success);
+// console.log(success.shift('ebwetrg'));
+// console.log(success);
+
+//adds and/or removes elements from an arrar.
+
+// ❓❓8 : challenge time
+
+// const months = ['jan' , 'march', 'april' , 'june' , 'july'];
+
+// 1: add dec at the end of an array?
+// //sol1:
+
+// const newMonth = months.splice(2,3,'dec' ); 
+// // [4=side , 0=delete , des=add word]
+// console.log(months);
+// console.log(months.length);
+
+// 2: what is the returen value of splice me thod?
+// const newMonth = months.splice(4,1,'dec' ); 
+// // [4=side , 0=delete , des=add word]
+// console.log(newMonth);
+
+
+// 3: update march to (update)?
+// 4: delete june from an array?
+
+// const  months = ['jan' , 'march', 'april' , 'june' , 'july'];
+
+// const indexofMonth = months.indexOf('april');
+// if(indexofMonth != -1 ){
+//   const updateMonths = months.splice(indexofMonth,Infinity);
+//   console.log(months);
+//   console.log(updateMonths);
+// }else{
+//   console.log('no such a data found');  
+// } 
+
+// 11:challenge time
+
+
+// 5️⃣ Array subsection 4 map and reduce method
+
+// array.prototype.map()
+
+// let newArray =arr.map (allback(currentValue,[index  [array]]) returen element for new Array,after executing }[,.thisArg]);
+
+// returns a new Array containing the returens of calling  a function on every element in this array.Array
+
+//const array1 = [1,4,9,16,25];
+// num > 9
+
+
+
+// it returnnew array without muting the orignal array
+ 
+// console.error("vasu patel");
+// array
+
+
+
+
+
+
+
+
+
+
+// Version: 1.79.2 (user setup)
+// Commit: 695af097c7bd098fbf017ce3ac85e09bbc5dda06
+// Date: 2023-06-14T08:57:04.  379Z
+// Electron: 22.5.7
+// Chromium: 108.0.5359.215
+// Node.js: 16.17.1
+// V8: 10.8.168.25-electron.0
+// OS: Windows_NT x64 10.0.22621
--- 01_basics/index.js
+++ 01_basics/index.js
@@ -1,10 +1,31 @@
 console.log("Vasu Patel");
 
-const accoundId = 1234;
+// const accoundId = 1234;
 
-let accountEmail = "vasupatel@gmail.com";
-let accountPassword = "12345";
-accountcity = "rajkot";
+// let accountEmail = "vasupatel@gmail.com";
+// let accountPassword = "12345";
+// accountCity = "rajkot";
 
-console.log(accountcity, accountEmail, accountPassword);
-console.table([accountEmail, accountPassword, accountcity]);
+// console.log(accountEmail, accountPassword, accountCity);
+// console.table([accountEmail, accountPassword, accountCity]);
+
+
+// let primes = [2, 3, 5, 7];
+// let prime = primes[primes.length - 2];
+
+// console.log(prime);
+
+
+// function fact(x) {
+//     if (x > 1) return x * fact(x - 1);
+//     else return 1;
+// }
+
+// let facts = fact(5);
+// console.log(facts);
+
+
+let x = 2, y = 3;
+let z = (x > 3) || (y < 3);
+
+console.log(z);
