5802879e32cbbef6eff827e5a413fac3f2b9bb0d
vasupatel2344 <vasupatel2344@gmail.com>
2024-10-11 09:46:37 +0530

    js book file

--- 01_basics/index.js
+++ 01_basics/index.js
@@ -9,23 +9,3 @@ console.log("Vasu Patel");
 // console.log(accountEmail, accountPassword, accountCity);
 // console.table([accountEmail, accountPassword, accountCity]);
 
-
-// let primes = [2, 3, 5, 7];
-// let prime = primes[primes.length - 2];
-
-// console.log(prime);
-
-
-// function fact(x) {
-//     if (x > 1) return x * fact(x - 1);
-//     else return 1;
-// }
-
-// let facts = fact(5);
-// console.log(facts);
-
-
-let x = 2, y = 3;
-let z = (x > 3) || (y < 3);
-
-console.log(z);
