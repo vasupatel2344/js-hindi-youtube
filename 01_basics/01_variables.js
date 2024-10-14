6220760b2bd944e06416dcd8fecd5a9a693db3a7
vasupatel2344 <vasupatel2344@gmail.com>
2024-10-14 15:44:02 +0530

    New File

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,11 +1,19 @@
 console.log("Vasu Patel");
 
-// const accoundId = 1234;
+const accoundId = 1234;
 
 // let accountEmail = "vasupatel@gmail.com";
 // let accountPassword = "12345";
 // accountCity = "rajkot";
 
+
 // console.log(accountEmail, accountPassword, accountCity);
 // console.table([accountEmail, accountPassword, accountCity]);
 
+// accountId = 2 // not allowed
+
+accountEmail = "v123@gmail.com";
+accountPassword = "1234567890";
+accountCity = "ahmedabad";
+let accountState;
+console.table([accoundId, accountEmail, accountPassword, accountCity, accountState]);
