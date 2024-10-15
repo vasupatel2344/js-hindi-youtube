c191eacc64e7461b7fb1a41a958e3b8026d37a51
vasupatel2344 <vasupatel2344@gmail.com>
2024-10-15 10:02:13 +0530

    dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,2 +1,42 @@
 // dataTypes  //
 
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello WORLD!" , 3 + 3 );  //we are using node js not browser
+
+
+console.log(3 + 3);
+
+let name = "vasu";
+let age = 18;
+let isLoggedIn = false;
+let state = null;
+
+
+
+console.log(name, typeof name);
+console.log(age, typeof age);
+console.log(isLoggedIn, typeof isLoggedIn);
+console.log(state, typeof state);
+
+let ages = 17;
+
+if (ages > 18) {
+    console.log("You can vote");
+} else {
+    console.log("You cannot vote");
+}
+
+// dataTypes =
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+// console.log(typeof null);
--- JAVASCRIPT the definitive guide/BookExe.js
+++ JAVASCRIPT the definitive guide/BookExe.js
@@ -1,17 +1,17 @@
 
-// let primes = [2, 3, 5, 7];
-// let prime = primes[primes.length - 2];
+let primes = [2, 3, 5, 7];
+let prime = primes[primes.length - 3];
 
-// console.log(prime);
+console.log(prime);
 
 
-// function fact(x) {
-//     if (x > 1) return x * fact(x - 1);
-//     else return 1;
-// }
+function fact(x) {
+    if (x > 1) return x * fact(x - 1);
+    else return 1;
+}
 
-// let facts = fact(5);
-// console.log(facts);
+let facts = fact(5);
+console.log(facts);
 
 
 let x = 2, y = 3;
