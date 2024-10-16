a33c80d6ad509ddea041cf8d66c6ba93850a5ab6
vasupatel2344 <vasupatel2344@gmail.com>
2024-10-16 17:05:27 +0530

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,37 @@
+// STRING  "string"
+
+const name = "vasu";
+const repoCount = 50;
+
+// console.log(name + repoCount + "Value");
+
+
+// console.log(`Hello my name is ${name.touppercase} and may repo count is ${repoCount}`);
+
+const gameName = new String('vasu_vegda')
+
+console.log(gameName);
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('a'));            // THIS IS METHOD ONLY
+// console.log(gameName.substring(0, 4));
+// console.log(gameName.slice(-10, 8));
+
+const newStringOne = "   vasu    ";
+// console.log( newStringOne);
+// console.log( newStringOne.trim);
+
+
+const url = "https://vasu=1223.com"
+
+console.log(url.replace('=', ''));
+
+
+url.includes('vasu') ? console.log("vasu is present") : console.log("vasu is not present");
+
+console.log(gameName.split('-'));
