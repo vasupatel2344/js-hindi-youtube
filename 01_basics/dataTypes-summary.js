c8f2413fd0720c63e4de60e8df056c256df6bcdc
vasupatel2344 <vasupatel2344@gmail.com>
2024-10-21 14:18:55 +0530

    datatype revision

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -1,36 +1,55 @@
 // primitives
 
-// 7 types : String,Number ,Boolean,Null,Undefined,Symbol,Bigint
-
+// 7 types primitives : String,Number ,Boolean,Null,Undefined,Symbol,Bigint
+// 3 types non primitives : Array, object, Function
 
 const score = 100
 const scoreValue = 100.3
 
+// console.log(score);             // 100
+// console.log(scoreValue);        // 100.3
+
+
 const isLoggedIn = false;
 const outsideTemp = null;
 let userEmail;
+// console.log(isLoggedIn);        // false
+// console.log(outsideTemp);       // null
+// console.log(userEmail);         // undefined
+
+const bigint = 1234n;
+// console.log(bigint);
+// console.log(typeof bigint);   
+
+
 
 const id = Symbol('123')
-const anotherId = Symbol('123');
+const Id = Symbol('123');
+
+// console.log(id === Id);
+
 
 // console.log(id === anotherId);
 
-const bigNumber = 456543576654356759n;
-// console.log(bigNumber);
 
 
 
-const heros = ["shaktiman", "naagraj", "doga"];
+const heros = {"shaktiman": "naagraj" };
+console.log(heros);
+console.log(typeof heros);
+console.log(typeof shaktiman);
 
-let myObj = {
+
+
+let myObj = { 
     name: "vasu",
     age: 22,
     isLoggedIn: true
 }
 
-// const myFunction = function () {
-//     console.log("Hello world");
-// }
+const myFunction = function  () {
+    console.log("Hello world");
+}
 
 // console.log(myFunction);
 
@@ -51,8 +70,8 @@ let myYoutubename = "vasupatel2344@gmail.com"
 let anothername = myYoutubename
 anothername = "chaiaurcode"
 
-console.log(myYoutubename);
-console.log(anothername);
+// console.log(myYoutubename);
+// console.log(anothername);
 
 let userOne = {
     email: "user@google.com",
@@ -63,7 +82,7 @@ let userTwo = userOne;
 
 userTwo.email = "vasu123@gmail.com"
 
-console.log(userOne.email);
-console.log(userTwo.email);
+// console.log(userOne.email);
+// console.log(userTwo.email);
 
 
